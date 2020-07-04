9546a5dadece82c106bd6338f7d97f56033037f6
Tony Inciarte <inciartet@gmail.com>
2020-07-04 18:24:52 -0400

    NewProject

---
+++ MyProject/MyProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyProject
+//
+//  Created by Tony Inciarte on 7/4/20.
+//  Copyright © 2020 Tony Inciarte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
